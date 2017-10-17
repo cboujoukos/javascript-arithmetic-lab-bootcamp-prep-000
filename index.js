adfc7465b7d6186ade2e1cc0365c3857f57d1702
Christopher Michael Boujoukos <cboujoukos@gmail.com>
2017-10-17 20:28:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ function add(a, b) {
 function subtract(a, b) {
   return a-=b
 }
+function multiply(a, b) {
+  return a*=b
+}
+function divide(a, b) {
+  return a/=b
+}
+function inc(n) {
+  return n++
+}
+function dec(n) {
+  return n--
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+funtion preserveDecimal(n) {
+  return parseFloat(n)
+}
